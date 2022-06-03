928c9e5bfbeb1aa4aa65a0a8ace3dc3320cfba8f
Ed Moore <eddie.moore@gmail.com>
2022-06-03 08:12:40 +0800

    prettier husky and lint-staged

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import Storage from "./rem-localstorage";
+import Storage from './rem-localstorage'
 
-let localStorage: Storage;
-if (typeof window === "undefined") {
-  const LocalStorage = require("node-localstorage").LocalStorage;
-  localStorage = new LocalStorage("./uls-scratch");
+let localStorage: Storage
+if (typeof window === 'undefined') {
+  const LocalStorage = require('node-localstorage').LocalStorage
+  localStorage = new LocalStorage('./uls-scratch')
 } else if (
-  typeof window.localStorage === "undefined" ||
-  typeof window.sessionStorage === "undefined"
+  typeof window.localStorage === 'undefined' ||
+  typeof window.sessionStorage === 'undefined'
 ) {
   // @ts-ignore
-  localStorage = new Storage("local");
+  localStorage = new Storage('local')
 } else {
-  localStorage = window.localStorage;
+  localStorage = window.localStorage
 }
-export default localStorage;
+export default localStorage
