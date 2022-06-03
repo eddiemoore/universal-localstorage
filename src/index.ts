2019fe390085795883eff253e7b865a0fb133c4d
Ed Moore <eddie.moore@gmail.com>
2022-06-03 11:05:48 +0800

    Use env var to specify nodes scratch folder #3

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import Storage from './rem-localstorage'
 
 let localStorage: Storage
 if (typeof window === 'undefined') {
-  const LocalStorage = require('node-localstorage').LocalStorage
-  localStorage = new LocalStorage('./uls-scratch')
+  const { LocalStorage } = require('node-localstorage')
+  localStorage = new LocalStorage(process.env.ULS_FOLDER || './uls-scratch')
 } else if (
   typeof window.localStorage === 'undefined' ||
   typeof window.sessionStorage === 'undefined'
